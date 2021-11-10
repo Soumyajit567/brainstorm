58b2292a5d4b5f688fc0513d7a32cddd191b79f2
ashopf <ashopf@iu.edu>
2021-11-09 20:50:41 -0500

    adding stuff to server.js

--- frontend/server.js
+++ frontend/server.js
@@ -11,3 +11,10 @@ app.use(express.static(path.join(__dirname, "build")));
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("/*", function(req, res) {
+        res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    });
+}
