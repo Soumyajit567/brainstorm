ff5081699c5b0b39787d784fa9b052ef3fe0d150
ashopf <ashopf@iu.edu>
2021-11-09 21:19:38 -0500

    changing server.js

--- frontend/server.js
+++ frontend/server.js
@@ -4,17 +4,13 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors');
 
 app.use(express.static(path.join(__dirname, "build")));
 
 // This route serves the React app
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-    app.get("/*", function(req, res) {
-        res.sendFile(path.join(__dirname, "./client/build/index.html"));
-    });
-}
+app.use(cors());
